f4ff769ff438c7e4acabaf95436f2e4686e95b6b
Kartik <kartikdroid17@gmail.com>
2019-04-13 19:04:28 +0530

    updated to add firebase imports

--- cartnavigator/src/index.js
+++ cartnavigator/src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { getFirestore } from 'redux-firestore'
-import { getFirebase } from 'react-redux-firebase'
-
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import firebaseConfig from './config/firebaseConfig'
+ 
+const store = createStore(rootReducer,
+    compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+    )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.unregister();
